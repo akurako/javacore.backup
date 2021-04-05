c13b5309986c1b3f76b107927d5be2dfc38df54a
Aleksejs Kurako <aleksejs.kurako@gmail.com>
2021-04-05 14:58:32 +0200

    First

---
+++ src/Backup.java
@@ -0,0 +1,5 @@
+import java.io.File;
+
+public class Backup {
+    public Backup(File source, File dest);
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] fileList;
+        File directoryName = new File("files");
+
+}
